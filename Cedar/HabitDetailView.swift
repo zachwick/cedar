9ece74765db8cade468e3833dce1a92cf193fd6a
zach wick <zach@zachwick.com>
2020-08-10 13:55:44 -0400

    Adds background, and niave dots implementation
    Signed-off-by: zach wick <zach@zachwick.com>

--- Cedar/HabitDetailView.swift
+++ Cedar/HabitDetailView.swift
@@ -8,22 +8,36 @@ struct HabitDetailView: View {
     
     var body: some View {
         NavigationView {
-            Button(action: {
-                self.habitsStore.deleteHabit(with: self.habitViewModel.id)
-                self.onClose()
-            }) {
-                Text("Delete")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.red)
+            ZStack {
+                Rectangle()
+                    .foregroundColor(Color(hue: 0.72, saturation: 0.02, brightness: 0.98))
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    HStack {
+                        ForEach(0..<5) { idx in
+                            Circle()
+                                .foregroundColor(habitViewModel.completionDaysAgo.contains(4 - idx) ? Color.cedarGreen : Color(.sRGB, white: 0, opacity: 0.10))
+                                .frame(width: 6, height: 6)
+                        }
+                    }
+                    Button(action: {
+                        self.habitsStore.deleteHabit(with: self.habitViewModel.id)
+                        self.onClose()
+                    }) {
+                        Text("Delete")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.red)
+                    }
+                    .navigationBarTitle(habitViewModel.title)
+                    .navigationBarItems(trailing: Button(action: {
+                        self.onClose()
+                    }, label: {
+                        Text("Close")
+                            .fontWeight(.bold)
+                            .foregroundColor(.cedarGreen)
+                    }))
+                }
             }
-            .navigationBarTitle(habitViewModel.title)
-            .navigationBarItems(trailing: Button(action: {
-                self.onClose()
-            }, label: {
-                Text("Close")
-                    .fontWeight(.bold)
-                    .foregroundColor(.cedarGreen)
-            }))
         }
     }
 }
